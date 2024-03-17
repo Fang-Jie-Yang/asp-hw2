04f316737d618cda3898f2aab2ebb6d0ae689f06
r12922015 <r12922015@ws4.csie.ntu.edu.tw>
2024-03-17 09:05:43 +0800

    add program for testing ioctls

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include "rootkit.h"
+
+int main(void) {
+
+	int fd;
+
+	fd = open("/dev/rootkit", O_RDWR);
+	if (fd < 0) {
+		fprintf(stderr, "failed to open /dev/rootkit: %s\n", strerror(errno));
+		exit(-1);
+	}
+
+	if (ioctl(fd, IOCTL_MOD_HIDE) < 0) {
+		fprintf(stderr, "ioctl failed\n");
+		exit(-1);
+	}
+
+	return 0;
+}
